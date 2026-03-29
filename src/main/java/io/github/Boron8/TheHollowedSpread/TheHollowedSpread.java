6c1a69aa671933fd4c5e213f1def0115fe402d9b
Boron8 <218486241+Boron8@users.noreply.github.com>
2026-03-29 11:33:22 +0200

    Added mod code

---
+++ src/main/java/io/github/Boron8/TheHollowedSpread/TheHollowedSpread.java
@@ -0,0 +1,26 @@
+package io.github.Boron8.TheHollowedSpread;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = TheHollowedSpread.MODID, name = TheHollowedSpread.NAME, version = TheHollowedSpread.VERSION)
+public class TheHollowedSpread {
+    public static final String MODID = "thehollowedspread";
+    public static final String NAME = "The Hollowed Spread";
+    public static final String VERSION = "1.0.0";
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+    }
+}
